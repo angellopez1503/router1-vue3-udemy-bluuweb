49bfd3cbd31b1853befedb86cefa0897c69d3c7e
Angel Lopez <angellopez15luisa@gmail.com>
2022-09-21 15:45:28 -0500

    add:change

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <nav class="navbar bg-dark navbar-dark">
+    <div class="container">
+      <router-link class="navbar-brand" to="/">
+        <img
+          src="@/assets/logo.svg"
+          alt="Logo"
+          width="30"
+          height="24"
+          class="d-inline-block align-text-top"
+        />
+        PokeAPI
+      </router-link>
+      <div>
+        <router-link active-class="active" class="btn btn-outline-primary me-2" to="/">Home</router-link>
+        <router-link active-class="active" class="btn btn-outline-primary" to="/pokemon">Pokemons</router-link>
+      </div>
+    </div>
+  </nav>
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/pokemon">Pokemon</RouterLink>
+      </nav>
+    </div>
+  </header> -->
+  <div class="container text-center">
+    <RouterView />
+  </div>
+  
+</template>
+
+<style scoped>
+ 
+</style>
---
+++ src/views/PokemonView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import axios from "axios";
+import { RouterLink } from "vue-router";
+import { useGetData } from '@/composables/getData';
+ 
+const { getData,data,loading,error} = useGetData()
+ 
+getData("https://pokeapi.co/api/v2/pokemon");
+</script>
+
+<template>
+  <h1>Pokemon</h1>
+  <p v-if="loading">Cargando informacion...</p>
+  <div class="alert alert-danger mt-2" v-if="error">{{ error }}</div>
+  <div v-if="data" class="mb-3">
+    <ul class="list-group mb-3">
+      <li class="list-group-item" v-for="item in data.results" :key="item.name">
+        <RouterLink :to="`/pokemon/${item.name}`">{{ item.name }}</RouterLink>
+      </li>
+    </ul>
+     <button :disabled="!data.previous" class="btn btn-success me-2" @click="getData(data.previous)">Previous</button>
+     <button :disabled="!data.next" class="btn btn-primary" @click="getData(data.next)">Next</button>
+  </div>
+</template>
